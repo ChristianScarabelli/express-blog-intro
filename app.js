0a2756686c4f7fdf8f7dcc291161c57737ae8b17
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-18 16:17:49 +0100

    ADD: json of blog posts on page

--- app.js
+++ app.js
@@ -9,12 +9,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('images'))  // rendo accessibile al server la cartella images 
+// rendo accessibile al server la cartella images 
+app.use(express.static('images'))
 
+// creo la rotta principale del mio server
 app.get('/', (req, res) => {
     res.send('Server del mio blog')
 })
 
+// creo la rotta bacheca che mi stampi in pagina il json dei post
+app.get('/bacheca', (req, res) => {
+    res.json(blogPosts)
+})
+
+// inizializzo il server
 app.listen(port, () => {
     console.log(`Il server del mio blog è in ascolto della porta: ${port}`)
 })
+
