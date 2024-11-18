a948d8fe1def67d9c39787cc32791cc2c0d3790c
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-18 15:35:00 +0100

    ADD: express init- express static folder

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // console.log('express-blog-intro')
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('images'))  // rendo accessibile al server la cartella images 
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+})
+
+app.listen(port, () => {
+    console.log(`Il server del mio blog è in ascolto della porta: ${port}`)
+})
